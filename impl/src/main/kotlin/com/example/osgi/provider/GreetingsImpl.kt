b29c170c8ad021d4e507bbcad52593ad0f4e45f9
Chris Rankin <chris.rankin@r3.com>
2020-08-28 21:14:51 +0100

    Add OSGi logging service, but log using SLF4J. No idea yet where these logs are going though.

--- impl/src/main/kotlin/com/example/osgi/provider/GreetingsImpl.kt
+++ impl/src/main/kotlin/com/example/osgi/provider/GreetingsImpl.kt
@@ -2,9 +2,20 @@ package com.example.osgi.provider
 
 import com.example.osgi.api.Greetings
 import org.osgi.service.component.annotations.Component
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @Component
+//class GreetingsImpl @Activate constructor(@Reference(service = LoggerFactory::class) logger: Logger) : Greetings {
 class GreetingsImpl : Greetings {
+    private companion object {
+        private val logger: Logger = LoggerFactory.getLogger(GreetingsImpl::class.java)
+    }
+
+    init {
+        logger.info("Activating Greetings")
+    }
+
     override fun greet(name: String): String {
         return "Hello $name!"
     }
