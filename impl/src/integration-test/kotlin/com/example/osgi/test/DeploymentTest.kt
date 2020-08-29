1b3f4733256f48338746a240bfc68e035272a549
Chris Rankin <chris.rankin@r3.com>
2020-08-29 01:48:07 +0100

    First attempt at injecting OSGi services into a test.

--- impl/src/integration-test/kotlin/com/example/osgi/test/DeploymentTest.kt
+++ impl/src/integration-test/kotlin/com/example/osgi/test/DeploymentTest.kt
@@ -1,9 +1,23 @@
 package com.example.osgi.test
 
+import com.example.osgi.api.Greetings
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
+import org.osgi.framework.FrameworkUtil
+import org.osgi.util.tracker.ServiceTracker
 
 class DeploymentTest {
     @Test
-    fun deploy() {
+    fun testGreet() {
+        assertEquals("Hello Bob!", greetings.greet("Bob"))
+    }
+
+    private val greetings: Greetings get() {
+        val bundle = FrameworkUtil.getBundle(DeploymentTest::class.java) ?: fail("Bundle not found!")
+        return with(ServiceTracker<Greetings, Greetings>(bundle.bundleContext, Greetings::class.java, null)) {
+            open()
+            waitForService(1000) ?: fail("No Greetings")
+        }
     }
 }
--- impl/src/main/kotlin/com/example/osgi/provider/GreetingsImpl.kt
+++ impl/src/main/kotlin/com/example/osgi/provider/GreetingsImpl.kt
@@ -2,10 +2,11 @@ package com.example.osgi.provider
 
 import com.example.osgi.api.Greetings
 import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.ServiceScope.SINGLETON
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-@Component
+@Component(scope = SINGLETON)
 //class GreetingsImpl @Activate constructor(@Reference(service = LoggerFactory::class) logger: Logger) : Greetings {
 class GreetingsImpl : Greetings {
     private companion object {
