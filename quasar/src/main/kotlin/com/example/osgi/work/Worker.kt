530abc43f7fad8f170cac562d8b387158355b968
Chris Rankin <chris.rankin@r3.com>
2020-09-04 15:15:37 +0100

    Try applying the Quasar agent to an OSGi test.

---
+++ quasar/src/main/kotlin/com/example/osgi/work/Worker.kt
@@ -0,0 +1,22 @@
+package com.example.osgi.work
+
+import co.paralleluniverse.fibers.Suspendable
+import com.example.osgi.api.Greetings
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Reference
+import org.osgi.service.log.Logger
+import org.osgi.service.log.LoggerFactory
+
+@Suppress("unused")
+@Component
+class Worker @Activate constructor(
+    @Reference(service = LoggerFactory::class)
+    private val logger: Logger
+) : Greetings {
+    @Suspendable
+    override fun greet(name: String): String {
+        logger.info("Greeting for worker {}", name)
+        return "You are suspended, $name!"
+    }
+}
