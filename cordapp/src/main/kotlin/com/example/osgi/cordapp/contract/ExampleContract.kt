b9756966cda24fbc1a70bf760c963d7788959322
Chris Rankin <chris.rankin@r3.com>
2020-09-29 19:34:19 +0100

    Create a CorDapp module to be loaded by a Quasar/Kotlin OSGi container.

---
+++ cordapp/src/main/kotlin/com/example/osgi/cordapp/contract/ExampleContract.kt
@@ -0,0 +1,40 @@
+package com.example.osgi.cordapp.contract
+
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.ContractState
+import net.corda.core.crypto.Crypto
+import net.corda.core.identity.AbstractParty
+import net.corda.core.transactions.LedgerTransaction
+import net.corda.core.utilities.OpaqueBytes
+import java.security.PublicKey
+
+@Suppress("unused")
+class ExampleContract : Contract {
+    override fun verify(tx: LedgerTransaction) {
+        val cryptoData = tx.outputsOfType<CryptoState>()
+        val validators = tx.commandsOfType<Validate>()
+
+        val isValid = validators.all { validate ->
+            with (validate.value) {
+                cryptoData.all { crypto ->
+                    Crypto.doVerify(schemeCodeName, publicKey, crypto.signature.bytes, crypto.original.bytes)
+                }
+            }
+        }
+
+        require(cryptoData.isNotEmpty() && validators.isNotEmpty() && isValid) {
+            "Failed to validate signatures in command data"
+        }
+    }
+
+    @Suppress("CanBeParameter", "MemberVisibilityCanBePrivate")
+    class CryptoState(val owner: AbstractParty, val original: OpaqueBytes, val signature: OpaqueBytes) : ContractState {
+        override val participants: List<AbstractParty> = listOf(owner)
+    }
+
+    class Validate(
+            val schemeCodeName: String,
+            val publicKey: PublicKey
+    ) : CommandData
+}
---
+++ cordapp/src/test/kotlin/com/example/osgi/cordapp/test/ExampleContractTest.kt
@@ -0,0 +1,9 @@
+package com.example.osgi.cordapp.test
+
+import org.junit.jupiter.api.Test
+
+class ExampleContractTest {
+    @Test
+    fun testCordapp() {
+    }
+}
