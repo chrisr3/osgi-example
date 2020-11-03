3cf5e5efd43ef06fa3335c762fcdb85a69811cf2
kriegfrj <fr.jkrieg@greekwelfaresa.org.au>
2020-11-04 09:57:32 +1030

    Fix for NCDFE in test when using CloseableBundleContext.

--- impl/src/integration-test/kotlin/com/example/osgi/test/DeploymentTest.kt
+++ impl/src/integration-test/kotlin/com/example/osgi/test/DeploymentTest.kt
@@ -1,14 +1,18 @@
 package com.example.osgi.test
 
+import kotlin.reflect.KClass;
 import com.example.osgi.api.Greetings
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.osgi.framework.ServiceListener
 import org.osgi.test.common.annotation.InjectService
 import org.osgi.test.junit5.service.ServiceExtension
 
 @ExtendWith(ServiceExtension::class)
 class DeploymentTest {
+
+    var listener: KClass<ServiceListener> = ServiceListener::class
 
     @InjectService(timeout = 1000)
     lateinit var greetings: Greetings
